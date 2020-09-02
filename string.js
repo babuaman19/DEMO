cbcca6c6ad3b285b0b045db702e700d4870c177e
Babu Aman Singh <65435598+babuaman19@users.noreply.github.com>
2020-09-02 12:37:00 +0530

    added string.html,string.js

---
+++ string.js
@@ -0,0 +1,5 @@
+var x = ["Hypertext", "Markup", "Language","is","the","standard","markup","language","for","documents","designed","to","be","displayed","in","a","web","browser"];
+console.log(x[4]);
+console.log(x[6]);
+console.log(x[8]);
+
